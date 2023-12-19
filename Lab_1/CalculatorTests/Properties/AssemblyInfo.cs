b70d237fe471d9ea292a674d4f8cdb6a4d46257a
StanislawKorol <118369137+StanislawKorol@users.noreply.github.com>
2023-12-19 12:43:32 +0200

    Add files via upload

---
+++ Lab_1/CalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e9325d8-75f0-49e3-8f9b-4e23106f5fd8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab_1/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using ErrorLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorTests
+{
+	[TestClass]
+	public class ABStest
+	{
+		public TestContext TestContext { get; set; }
+		[DataSource("System.Data.SqlClient", @"Data Source=(local)\SQLEXPRESS;Initial Catalog=CalcDatabase;Integrated Security=True", "ABS", DataAccessMethod.Sequential)]
+		[TestMethod]
+		public void ABSmethod()
+		{
+			try
+			{
+				long test_value = Convert.ToInt64(TestContext.DataRow["test_value"]);
+				long expected_value = 0;
+				try
+				{
+					 expected_value = Convert.ToInt64(TestContext.DataRow["result_value"]);
+				}
+				catch
+				{ }
+
+				int result = CalcClassBr.CalcClass.ABS(test_value);
+				Assert.AreEqual(expected_value, result);
+			}
+			catch (ArgumentOutOfRangeException ex) {
+				Assert.AreEqual(ErrorsExpression.ERROR_06, ex.ParamName);
+			}
+		}
+	}
+}
